5e689667bce7dc0cd00d83dddb2f9c935132a1a5
TuhkanenS <tuhkanenssss@gmail.com>
2026-05-16 12:05:18 +0300

    Fix API's registered

--- src/main/kotlin/dev/nautilus/devapi/core/NauDevAPILoader.kt
+++ src/main/kotlin/dev/nautilus/devapi/core/NauDevAPILoader.kt
@@ -1,11 +1,15 @@
 package dev.nautilus.devapi.core
 
+import dev.nautilus.devapi.manager.config.ConfigManager
+import dev.nautilus.devapi.manager.config.api.ConfigAPI
 import dev.nautilus.devapi.manager.features.PathsFeature
 import dev.nautilus.devapi.manager.file.FileManager
 import dev.nautilus.devapi.manager.file.api.FileAPI
 import dev.nautilus.devapi.manager.instance.InstanceManager
 import dev.nautilus.devapi.manager.instance.api.InstanceAPI
 import dev.nautilus.devapi.manager.lang.LangFileManager
+import dev.nautilus.devapi.manager.lang.LangManager
+import dev.nautilus.devapi.manager.lang.api.LangAPI
 import dev.nautilus.devapi.manager.lang.api.LangFileAPI
 
 internal object NauDevAPILoader {
@@ -17,7 +21,9 @@ internal object NauDevAPILoader {
     private fun setupAPI() {
         NauDevAPI.registerAPI<InstanceAPI>(InstanceManager())
         NauDevAPI.registerAPI<LangFileAPI>(LangFileManager())
+        NauDevAPI.registerAPI<LangAPI>(LangManager())
         NauDevAPI.registerAPI<FileAPI>(FileManager())
+        NauDevAPI.registerAPI<ConfigAPI>(ConfigManager())
     }
 
     private fun setupFeatures() {
