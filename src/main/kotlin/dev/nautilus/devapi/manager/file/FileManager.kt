dd6d448b1d5ba89e262191c711a7616428831ed1
TuhkanenS <tuhkanenssss@gmail.com>
2026-05-16 12:24:15 +0300

    Fix bugs mkdirs

--- src/main/kotlin/dev/nautilus/devapi/manager/file/FileManager.kt
+++ src/main/kotlin/dev/nautilus/devapi/manager/file/FileManager.kt
@@ -24,7 +24,7 @@ class FileManager : FileAPI {
         val resourcePath = Path.of(resourcePath, fileName).toString()
         val targetFile = dataFolder.resolve(resourcePath)
 
-        targetFile.parentFile.mkdir()
+        targetFile.parentFile.mkdirs()
 
         if (!targetFile.exists()) {
             plugin.getResource(resourcePath)?.use { input ->
